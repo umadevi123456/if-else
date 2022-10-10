3c626750517c07fef87266ee7615e7ceb8e1a831
umadevi123456 <umadevi22@navgurukul.org>
2022-10-10 14:44:18 +0530

    first commit

---
+++ ATM .py
@@ -0,0 +1,120 @@
+# balance=500000
+# print("welcome to SBI bank")
+# print("insert your card")
+# print("select your language")
+# language=input("enter language:")
+# if language=="english":
+#     password=int(input("enter password:"))
+#     if password==1234:
+#         print("1=withdrawal")
+#         print("2=balance_enquiry")
+#         print("3=pin generation ")
+#         print("4=deposite")
+#         print("5=ministatement")
+#         print("6=exit")
+#         transaction=int(input("enter transaction:"))
+#         if transaction==1:
+#             withdrawal=int(input("enter the withdrawal1:"))
+#             if withdrawal<=balance:
+#                 print("transaction successfilly","collect your withdrawal cash")
+#                 print("remaining balance",balance-withdrawal)
+#             else:
+#                 print("please enter correct amount")
+#         elif transaction==2:
+#             balance_enquiry=(input("saving/current:"))
+#             if balance_enquiry=="saving":
+#                 print("your balance",balance)
+#             else:
+#                 print("enter correct values")
+#         elif transaction==3:
+#             pin_genaration=int(input("enter new pin:"))
+#             if password==password:
+#                 print("new pin generated successfully")
+#             else:
+#                 print("not correct")
+#         elif transaction==4:
+#             deposite=int(input("enter the deposite money:"))
+#             if deposite>=10:
+#                 print("total money is",deposite+balance)
+#                 print("money is deposited successfully")
+#             else:
+#                 print("no deposite")
+#         elif transaction==5:
+#             ministatement=input("kcc/current/saving:")
+#             if ministatement=="kcc":
+#                 print("take your ministatement",ministatement)
+#             else:
+#                 print("not coming ministatement")
+#         elif transaction==6:
+#             exit=input("do you want to exit:")
+#             if exit=="yes":
+#                 print("thanku for visiting")
+#             else:
+#                 print("choose your transaction")
+#         else:
+#             print("please enter proper value")
+#     else:
+#         print("enter correct password")    
+# else:
+#     print("language doesnt exist")
+
+
+# balance=50000
+# print("welcome SBI Bank")
+# print("swipe your card")
+# print("select your language")
+# print("enter password")
+# language=input("enter language")
+# if language=="english":
+#     password=int(input("enter password"))
+#     if password==1234:
+#         print("1=withdrawal")
+#         print("2=deposite") 
+#         print("3=balance_enquary")
+#         print("4=pin_genration")
+#         print("5= exit")
+#         transaction=int(input("enter transaction:"))
+#         withdrawal=int(input("enter withdrawal1="))
+#         if transaction=="1":
+#             if withdrawal<=balance:
+#                 print("collect your cash",withdrawal)
+#                 print("remaining balance",balance-withdrawal)
+#             else:
+#                 print("please enter correct amount")
+#                 if transaction=="2":
+#                     deposite=int(input("enter deposite amount"))
+#                     if deposite>="5000":
+#                         print("total money is",deposite+balance)
+#                         print("deposite successfully")
+#                     else:
+#                         print("no deposite")
+#                         if transaction=="3":
+#                             balance_enquary=input("savings/current")
+#                             if balance_enquary=="savings":
+#                                 print("your balance",balance)
+#                             else:
+#                                 print("enter correct values")
+#                                 if transaction=="4":
+#                                    pin_genaration=int(input("enter new pin"))
+#                                    if password=="password":
+#                                        print("new pin generated successfully")
+#                                    else:
+#                                        print("not correct")
+#                                        if transaction=="5":
+#                                            exit=input("do you want to exit")
+#                                            if exit=="yes":
+#                                                print("thank you for visiting")
+#                                            else:
+#                                                print("choose your transaction")
+#     else:
+#         print("enter correct values")
+# else:
+#     print("language doesnot exits")
+
+
+
+
+
+
+
+
